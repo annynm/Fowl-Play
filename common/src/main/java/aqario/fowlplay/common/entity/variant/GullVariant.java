dd34d1135010d3760ee071cd1d1272b6b94b8460
Annyn Matheson <annynm@gmail.com>
2026-05-27 12:56:22 -0700

    change import ResourceLocation to Identifier for 1.21.11

--- common/src/main/java/aqario/fowlplay/common/entity/variant/GullVariant.java
+++ common/src/main/java/aqario/fowlplay/common/entity/variant/GullVariant.java
@@ -7,25 +7,23 @@ import net.minecraft.core.Holder;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 
-public record GullVariant(ResourceLocation texture) {
-    public static final StreamCodec<RegistryFriendlyByteBuf, Holder<GullVariant>> PACKET_CODEC = ByteBufCodecs.holderRegistry(
-        FPRegistries.GULL_VARIANT
-    );
-    public static final CommonRegister<GullVariant> REGISTRAR = CommonRegister.create(
-        FPRegistries.GULL_VARIANT,
-        FowlPlay.ID
-    );
-    public static final ResourceKey<GullVariant> HERRING = register("herring");
-    public static final ResourceKey<GullVariant> RING_BILLED = register("ring_billed");
-    public static final ResourceKey<GullVariant> BLACK_BACKED = register("black_backed");
+public record GullVariant(Identifier texture) {
+  public static final StreamCodec<RegistryFriendlyByteBuf, Holder<GullVariant>> PACKET_CODEC =
+      ByteBufCodecs.holderRegistry(FPRegistries.GULL_VARIANT);
+  public static final CommonRegister<GullVariant> REGISTRAR =
+      CommonRegister.create(FPRegistries.GULL_VARIANT, FowlPlay.ID);
+  public static final ResourceKey<GullVariant> HERRING = register("herring");
+  public static final ResourceKey<GullVariant> RING_BILLED = register("ring_billed");
+  public static final ResourceKey<GullVariant> BLACK_BACKED = register("black_backed");
 
-    private static ResourceKey<GullVariant> register(String id) {
-        ResourceKey<GullVariant> key = ResourceKey.create(FPRegistries.GULL_VARIANT, FowlPlay.id(id));
-        ResourceLocation texture = FowlPlay.id("textures/entity/gull/" + key.location().getPath() + "_gull.png");
-        REGISTRAR.register(id, () -> new GullVariant(texture));
-        return key;
-    }
+  private static ResourceKey<GullVariant> register(String id) {
+    ResourceKey<GullVariant> key = ResourceKey.create(FPRegistries.GULL_VARIANT, FowlPlay.id(id));
+    Identifier texture =
+        FowlPlay.id("textures/entity/gull/" + key.location().getPath() + "_gull.png");
+    REGISTRAR.register(id, () -> new GullVariant(texture));
+    return key;
+  }
 }
--- common/src/main/java/aqario/fowlplay/common/entity/variant/SparrowVariant.java
+++ common/src/main/java/aqario/fowlplay/common/entity/variant/SparrowVariant.java
@@ -7,24 +7,23 @@ import net.minecraft.core.Holder;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 
-public record SparrowVariant(ResourceLocation texture) {
-    public static final StreamCodec<RegistryFriendlyByteBuf, Holder<SparrowVariant>> PACKET_CODEC = ByteBufCodecs.holderRegistry(
-        FPRegistries.SPARROW_VARIANT
-    );
-    public static final CommonRegister<SparrowVariant> REGISTRAR = CommonRegister.create(
-        FPRegistries.SPARROW_VARIANT,
-        FowlPlay.ID
-    );
-    public static final ResourceKey<SparrowVariant> BROWN = register("brown");
-    public static final ResourceKey<SparrowVariant> PALE = register("pale");
+public record SparrowVariant(Identifier texture) {
+  public static final StreamCodec<RegistryFriendlyByteBuf, Holder<SparrowVariant>> PACKET_CODEC =
+      ByteBufCodecs.holderRegistry(FPRegistries.SPARROW_VARIANT);
+  public static final CommonRegister<SparrowVariant> REGISTRAR =
+      CommonRegister.create(FPRegistries.SPARROW_VARIANT, FowlPlay.ID);
+  public static final ResourceKey<SparrowVariant> BROWN = register("brown");
+  public static final ResourceKey<SparrowVariant> PALE = register("pale");
 
-    private static ResourceKey<SparrowVariant> register(String id) {
-        ResourceKey<SparrowVariant> key = ResourceKey.create(FPRegistries.SPARROW_VARIANT, FowlPlay.id(id));
-        ResourceLocation texture = FowlPlay.id("textures/entity/sparrow/" + key.location().getPath() + "_sparrow.png");
-        REGISTRAR.register(id, () -> new SparrowVariant(texture));
-        return key;
-    }
+  private static ResourceKey<SparrowVariant> register(String id) {
+    ResourceKey<SparrowVariant> key =
+        ResourceKey.create(FPRegistries.SPARROW_VARIANT, FowlPlay.id(id));
+    Identifier texture =
+        FowlPlay.id("textures/entity/sparrow/" + key.location().getPath() + "_sparrow.png");
+    REGISTRAR.register(id, () -> new SparrowVariant(texture));
+    return key;
+  }
 }
