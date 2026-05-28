a9ed4e71f6028c30a47c079a327f42e63d0f09f7
Annyn Matheson <annynm@gmail.com>
2026-05-28 11:59:47 -0700

    Layers refactored into abstract BirdLayer class

--- common/src/main/java/aqario/fowlplay/client/RenderRegistry.java
+++ common/src/main/java/aqario/fowlplay/client/RenderRegistry.java
@@ -1,6 +1,8 @@
 package aqario.fowlplay.client;
 
+import aqario.fowlplay.client.render.entity.layer.*;
 import dev.architectury.injectables.annotations.ExpectPlatform;
+import java.util.function.Supplier;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.builders.LayerDefinition;
 import net.minecraft.client.particle.ParticleProvider;
@@ -11,26 +13,55 @@ import net.minecraft.core.particles.ParticleType;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 
-import java.util.function.Supplier;
-
 public class RenderRegistry {
-    @ExpectPlatform
-    public static <T extends Entity> void entityRenderer(Supplier<EntityType<T>> type, EntityRendererProvider<T> provider) {
-        throw new AssertionError();
-    }
 
-    @ExpectPlatform
-    public static void modelLayer(ModelLayerLocation location, Supplier<LayerDefinition> definition) {
-        throw new AssertionError();
-    }
+  /**
+   * Registers all entity model layers for every bird entity. Call this during client
+   * initialization.
+   */
+  public static void registerAllLayers() {
+    // Penguins
+    modelLayer(PenguinLayer.MODEL_LAYER, PenguinLayer::createLayerDefinition);
+    modelLayer(BabyPenguinLayer.MODEL_LAYER, BabyPenguinLayer::createLayerDefinition);
 
-    @ExpectPlatform
-    public static <T extends ParticleOptions, P extends ParticleType<T>> void particleFactory(Supplier<P> supplier, WrappedParticleProvider<T> provider) {
-        throw new AssertionError();
-    }
+    // TODO: Add remaining bird layers as they are migrated:
+    // modelLayer(DuckLayer.MODEL_LAYER, DuckLayer::createLayerDefinition);
+    // modelLayer(GooseLayer.MODEL_LAYER, GooseLayer::createLayerDefinition);
+    // modelLayer(GullLayer.MODEL_LAYER, GullLayer::createLayerDefinition);
+    // modelLayer(SparrowLayer.MODEL_LAYER, SparrowLayer::createLayerDefinition);
+    // modelLayer(PigeonLayer.MODEL_LAYER, PigeonLayer::createLayerDefinition);
+    // modelLayer(RobinLayer.MODEL_LAYER, RobinLayer::createLayerDefinition);
+    // modelLayer(CardinalLayer.MODEL_LAYER, CardinalLayer::createLayerDefinition);
+    // modelLayer(BlueJayLayer.MODEL_LAYER, BlueJayLayer::createLayerDefinition);
+    // modelLayer(CrowLayer.MODEL_LAYER, CrowLayer::createLayerDefinition);
+    // modelLayer(RavenLayer.MODEL_LAYER, RavenLayer::createLayerDefinition);
+    // modelLayer(HawkLayer.MODEL_LAYER, HawkLayer::createLayerDefinition);
+    // modelLayer(VultureLayer.MODEL_LAYER, VultureLayer::createLayerDefinition);
+    // modelLayer(ChickadeeLayer.MODEL_LAYER, ChickadeeLayer::createLayerDefinition);
+    // modelLayer(ChickenLayer.MODEL_LAYER, ChickenLayer::createLayerDefinition);
+    // modelLayer(BabyChickenLayer.MODEL_LAYER, BabyChickenLayer::createLayerDefinition);
+    // modelLayer(ScarecrowLayer.MODEL_LAYER, ScarecrowLayer::createLayerDefinition);
+  }
 
-    @FunctionalInterface
-    public interface WrappedParticleProvider<T extends ParticleOptions> {
-        ParticleProvider<T> create(SpriteSet spriteSet);
-    }
+  @ExpectPlatform
+  public static <T extends Entity> void entityRenderer(
+      Supplier<EntityType<T>> type, EntityRendererProvider<T> provider) {
+    throw new AssertionError();
+  }
+
+  @ExpectPlatform
+  public static void modelLayer(ModelLayerLocation location, Supplier<LayerDefinition> definition) {
+    throw new AssertionError();
+  }
+
+  @ExpectPlatform
+  public static <T extends ParticleOptions, P extends ParticleType<T>> void particleFactory(
+      Supplier<P> supplier, WrappedParticleProvider<T> provider) {
+    throw new AssertionError();
+  }
+
+  @FunctionalInterface
+  public interface WrappedParticleProvider<T extends ParticleOptions> {
+    ParticleProvider<T> create(SpriteSet spriteSet);
+  }
 }
--- common/src/main/java/aqario/fowlplay/client/render/entity/model/BirdModel.java
+++ common/src/main/java/aqario/fowlplay/client/render/entity/model/BirdModel.java
@@ -1,69 +1,81 @@
 package aqario.fowlplay.client.render.entity.model;
 
 import aqario.fowlplay.common.entity.bird.BirdEntity;
-import net.minecraft.client.model.HierarchicalModel;
+import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.util.Mth;
 
-public abstract class BirdModel<E extends BirdEntity> extends HierarchicalModel<E> {
-    public final ModelPart root;
-    public final ModelPart body;
-    public final ModelPart neck;
-    public final ModelPart head;
-    public final ModelPart torso;
-    public final ModelPart leftWing;
-    public final ModelPart rightWing;
-    public final ModelPart leftLeg;
-    public final ModelPart rightLeg;
-    public final ModelPart tail;
+public abstract class BirdModel<E extends BirdEntity> extends EntityModel<E> {
+  public final ModelPart root;
+  public final ModelPart body;
+  public final ModelPart neck;
+  public final ModelPart head;
+  public final ModelPart torso;
+  public final ModelPart leftWing;
+  public final ModelPart rightWing;
+  public final ModelPart leftLeg;
+  public final ModelPart rightLeg;
+  public final ModelPart tail;
 
-    public BirdModel(ModelPart root) {
-        this.root = root.getChild("root");
-        this.body = this.root.getChild("body");
-        this.neck = this.body.getChild("neck");
-        this.head = this.neck.getChild("head");
-        this.torso = this.body.getChild("torso");
-        this.leftWing = this.body.getChild("left_wing");
-        this.rightWing = this.body.getChild("right_wing");
-        this.leftLeg = this.root.getChild("left_leg");
-        this.rightLeg = this.root.getChild("right_leg");
-        this.tail = this.body.getChild("tail");
+  public BirdModel(ModelPart root) {
+    this.root = root.getChild("root");
+    this.body = this.root.getChild("body");
+    this.neck = this.body.getChild("neck");
+    this.head = this.neck.getChild("head");
+    this.torso = this.body.getChild("torso");
+    this.leftWing = this.body.getChild("left_wing");
+    this.rightWing = this.body.getChild("right_wing");
+    this.leftLeg = this.root.getChild("left_leg");
+    this.rightLeg = this.root.getChild("right_leg");
+    this.tail = this.body.getChild("tail");
+  }
+
+  @Override
+  public ModelPart root() {
+    return this.root;
+  }
+
+  @Override
+  public final void setupAnim(
+      E entity,
+      float limbSwing,
+      float limbSwingAmount,
+      float ageInTicks,
+      float netHeadYaw,
+      float headPitch) {}
+
+  @Override
+  public void prepareMobModel(E entity, float limbSwing, float limbSwingAmount, float partialTick) {
+    this.root().getAllParts().forEach(ModelPart::resetPose);
+    float ageInTicks = entity.tickCount + partialTick;
+    float bodyYaw = Mth.rotLerp(partialTick, entity.yBodyRotO, entity.yBodyRot);
+    float headYaw = Mth.rotLerp(partialTick, entity.yHeadRotO, entity.yHeadRot);
+    float relativeHeadYaw = Mth.wrapDegrees(headYaw - bodyYaw);
+
+    float headPitch = Mth.lerp(partialTick, entity.xRotO, entity.getXRot());
+    if (LivingEntityRenderer.isEntityUpsideDown(entity)) {
+      headPitch *= -1.0F;
+      relativeHeadYaw *= -1.0F;
     }
 
-    @Override
-    public ModelPart root() {
-        return this.root;
-    }
+    this.setAnimations(
+        entity, limbSwing, limbSwingAmount, ageInTicks, relativeHeadYaw, headPitch, partialTick);
+  }
 
-    @Override
-    public final void setupAnim(E entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
-    }
+  protected void setAnimations(
+      E entity,
+      float limbSwing,
+      float limbSwingAmount,
+      float ageInTicks,
+      float netHeadYaw,
+      float headPitch,
+      float partialTick) {}
 
-    @Override
-    public void prepareMobModel(E entity, float limbSwing, float limbSwingAmount, float partialTick) {
-        this.root().getAllParts().forEach(ModelPart::resetPose);
-        float ageInTicks = entity.tickCount + partialTick;
-        float bodyYaw = Mth.rotLerp(partialTick, entity.yBodyRotO, entity.yBodyRot);
-        float headYaw = Mth.rotLerp(partialTick, entity.yHeadRotO, entity.yHeadRot);
-        float relativeHeadYaw = Mth.wrapDegrees(headYaw - bodyYaw);
-
-        float headPitch = Mth.lerp(partialTick, entity.xRotO, entity.getXRot());
-        if(LivingEntityRenderer.isEntityUpsideDown(entity)) {
-            headPitch *= -1.0F;
-            relativeHeadYaw *= -1.0F;
-        }
-
-        this.setAnimations(entity, limbSwing, limbSwingAmount, ageInTicks, relativeHeadYaw, headPitch, partialTick);
-    }
-
-    protected void setAnimations(E entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float partialTick) {
-    }
-
-    protected void updateHeadRotation(float headYaw, float headPitch) {
-        headYaw = Mth.clamp(headYaw, -135.0F, 135.0F);
-        headPitch = Mth.clamp(headPitch, -45.0F, 45.0F);
-        this.neck.yRot = headYaw * (float) (Math.PI / 180.0);
-        this.neck.xRot = headPitch * (float) (Math.PI / 180.0);
-    }
+  protected void updateHeadRotation(float headYaw, float headPitch) {
+    headYaw = Mth.clamp(headYaw, -135.0F, 135.0F);
+    headPitch = Mth.clamp(headPitch, -45.0F, 45.0F);
+    this.neck.yRot = headYaw * (float) (Math.PI / 180.0);
+    this.neck.xRot = headPitch * (float) (Math.PI / 180.0);
+  }
 }
